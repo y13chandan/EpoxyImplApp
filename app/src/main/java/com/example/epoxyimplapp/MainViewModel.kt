f0585a907d642f43cdfe707ead38a56d051977a9
y13chandan <apple@Apples-MacBook-Pro.local>
2021-10-27 19:16:19 +0530

    val featuredProducts = liveData(Dispatchers.IO) {

--- app/src/main/java/com/example/epoxyimplapp/MainViewModel.kt
+++ app/src/main/java/com/example/epoxyimplapp/MainViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.epoxyimplapp
 
+import androidx.core.graphics.PathUtils.flatten
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
 
 class MainViewModel: ViewModel() {
-
+    val featuredProducts = liveData(Dispatchers.IO) {
+        emit(resultOf {
+            val products = EpoxyImplApp.appRepository.fetchData().flatMap { flatten(it) }
+            products.shuffled().take(20)
+        })
+    }
 }
